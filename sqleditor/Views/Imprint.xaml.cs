620193b3e85c2ac8a9a93312433783e4327e869b
FabriceGWP <schaubfabrice@gmail.com>
2025-01-09 10:47:23 +0100

    added imprint

--- sqleditor/Views/Imprint.xaml.cs
+++ sqleditor/Views/Imprint.xaml.cs
@@ -12,5 +12,10 @@ namespace sqleditor.Views
         {
             InitializeComponent();
         }
+
+        private async void OnBackButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
     }
 }
